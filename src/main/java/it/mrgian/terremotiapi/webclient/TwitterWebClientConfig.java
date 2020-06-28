d32cd9950361905366dc7375a955f2e8bbfae814
mrgian <gianmatteo.palmieri@gmail.com>
2020-06-28 15:26:10 +0200

    Aggiunta classe di configurazione web client

---
+++ src/main/java/it/mrgian/terremotiapi/webclient/TwitterWebClientConfig.java
@@ -0,0 +1,90 @@
+package it.mrgian.terremotiapi.webclient;
+
+import java.io.*;
+
+import com.fasterxml.jackson.databind.*;
+
+import org.springframework.util.ResourceUtils;
+
+/**
+ * Classe per la gestione della configurazione del web client
+ * 
+ * @author Gianmatteo Palmieri
+ */
+public class TwitterWebClientConfig {
+    private String baseUrl;
+    private String token;
+
+    /**
+     * Questo costruttore viene utilizzato quando vengono forniti il baseUrl e il
+     * token
+     * 
+     * @param baseUrl Url base della API di Twitter
+     * @param token   Token per l'autenticazione alla API di Twitter
+     */
+    public TwitterWebClientConfig(String baseUrl, String token) {
+        this.setBaseUrl(baseUrl);
+        this.setToken(token);
+    }
+
+    /**
+     * Questo costruttore viene utilizzato quando NON vengono forniti il baseUrl e
+     * il token e vengono utilizzati quelli di default <br>
+     * I valori di default vengono letti dal file JSON <b>defaultConfig.json</b>
+     * presente nella directory <b>resources</b> del progetto.
+     */
+    public TwitterWebClientConfig() {
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode;
+            jsonNode = objectMapper.readValue(getDefaultConfigJson(), JsonNode.class);
+
+            this.setBaseUrl(jsonNode.get("baseUrl").textValue());
+            this.setToken(jsonNode.get("token").textValue());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Metodo per leggere dal file <b>defaultConfig.json</b> il baseUrl e il token
+     * di default
+     * 
+     * @return il contenuto del file <b>defaultConfig.json</b>
+     */
+    private String getDefaultConfigJson() {
+        String config = "";
+
+        try {
+            File configFile = ResourceUtils.getFile("classpath:defaultConfig.json");
+            BufferedReader reader = new BufferedReader(new FileReader(configFile));
+
+            String line;
+            while ((line = reader.readLine()) != null)
+                config += line + "\n";
+
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return config;
+    }
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+
+    public void setBaseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
