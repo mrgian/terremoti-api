0fff055d100c269dd7505b95f3db35f79232d3c1
mrgian <gianmatteo.palmieri@gmail.com>
2020-06-29 12:34:50 +0200

    Rimossi import inutilizzati

--- src/main/java/it/mrgian/terremotiapi/Main.java
+++ src/main/java/it/mrgian/terremotiapi/Main.java
@@ -1,13 +1,7 @@
 package it.mrgian.terremotiapi;
 
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import it.mrgian.terremotiapi.model.Terremoto;
-import it.mrgian.terremotiapi.webclient.TwitterWebClient;
-import it.mrgian.terremotiapi.webclient.config.TwitterWebClientConfig;
 
 @SpringBootApplication
 public class Main {
